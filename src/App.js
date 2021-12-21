e51b5e4cbc865cfd4424d29dc38d8acd9f62937e
Carlos <cajavi89@gmail.com>
2021-12-21 17:44:39 -0500

    Site Finished

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import './App.css';
-import {useContext} from 'react'
 import { ItemContextProvider } from './context/itemContext';
-import { ItemContext } from './context/itemContext';
+import { useEffect,useState } from 'react';
+import {Routes,Route, Link} from 'react-router-dom'
 import AllProducts from './components/AllProducts';
+import About from './components/About';
+import ProductDetail from './components/ProductDetail'
+import CallFakeAPI from './helpers/CallFakeAPI'
+import Footer from './components/Footer';
 
 
 function App() {
-  // const {storeItems} = useContext(ItemContext)
-    // console.log(storeItems);
-
+  const [product,setProduct] = useState([])
+  useEffect(()=>{
+    CallFakeAPI(12)
+      .then(res => setProduct(res))
+  },[setProduct])
 
   return (
     <ItemContextProvider>
-    <div className="App">
-      <AllProducts />
-    </div>
+      <div class="app">
+        <nav className='menu'>
+          <Link to= '/'>Home</Link>
+          <Link to= '/about'>About</Link>
+        </nav>
+
+        <Routes>
+          <Route path='/'
+            element=
+            {
+              <>
+                <h1>Super Store</h1>
+                <AllProducts />
+              </>
+            }
+          />
+          <Route path='/about' element= {<About />} />
+          <Route path='/product/:productId'
+            element= {
+              <ProductDetail data={product}/>
+            }
+          />
+        </Routes>
+        <Footer />
+      </div>
     </ItemContextProvider>
   );
 }
+
+
 
 export default App;
